1e6cf6ca4c79f4b088fbb0df42a906be8ce9fe6b
Geass <geass1000@gmail.com>
2018-02-22 13:58:08 +0300

    add: write methods for simple-ack

--- src/core/layers/apdus/simple-ack.pdu.ts
+++ src/core/layers/apdus/simple-ack.pdu.ts
@@ -1,8 +1,24 @@
 import * as _ from 'lodash';
 
-import { OffsetUtil, TyperUtil, BACnetReaderUtil } from '../../utils';
+import {
+    OffsetUtil,
+    TyperUtil,
+    BACnetReaderUtil,
+    BACnetWriterUtil,
+} from '../../utils';
 
-import { BACNET_CONFIRMED_SERVICE } from '../../enums/service.enum';
+import {
+    BACNET_PROP_TYPES,
+    BACNET_TAG_TYPES,
+    BACNET_CONFIRMED_SERVICE,
+    BACNET_UNCONFIRMED_SERVICE,
+    BACNET_SERVICE_TYPES,
+} from '../../enums';
+
+import {
+    ISimpleACK,
+    ISimpleACKSubscribeCOV,
+} from '../../interfaces';
 
 export class SimpleACKPDU {
     constructor () {
@@ -49,5 +65,41 @@ export class SimpleACKPDU {
         const serviceMap: Map<string, any> = new Map();
 
         return serviceMap;
+    }
+
+    /**
+     * writeReq - writes the massage for simple ack (header).
+     *
+     * @param  {ISimpleACK} params - SimpleACK params
+     * @return {BACnetWriterUtil}
+     */
+    public writeReq (params: ISimpleACK): BACnetWriterUtil {
+        const writer = new BACnetWriterUtil();
+
+        // Write Service Type
+        const mMeta = TyperUtil.setBitRange(0x00,
+            BACNET_SERVICE_TYPES.SimpleACKPDU, 4, 4);
+        writer.writeUInt8(mMeta);
+
+        // Write InvokeID
+        writer.writeUInt8(params.invokeId);
+
+        return writer;
+    }
+
+    /**
+     * writeSubscribeCOV - writes the message for SubscribeCOV service and
+     * returns the instance of the writer utility.
+     *
+     * @param  {ISimpleACKSubscribeCOV} params - SubscribeCOV params
+     * @return {BACnetWriterUtil}
+     */
+    public writeSubscribeCOV (params: ISimpleACKSubscribeCOV): BACnetWriterUtil {
+        const writer = new BACnetWriterUtil();
+
+        // Write Service choice
+        writer.writeUInt8(BACNET_CONFIRMED_SERVICE.SubscribeCOV);
+
+        return writer;
     }
 }
