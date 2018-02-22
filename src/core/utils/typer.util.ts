5d27f1956a7315adfda3d9b81b861edf6fdd4f5d
Geass <geass1000@gmail.com>
2018-02-22 13:26:38 +0300

    add: comments to TyperUtil methods

--- src/core/utils/typer.util.ts
+++ src/core/utils/typer.util.ts
@@ -4,25 +4,46 @@ import { ApiError } from '../errors';
 
 export class TyperUtil {
 
-    static setBit (numValue: number, pos: number, bitValue: boolean): number {
+    /**
+     * setBit - sets the bit value in specific position.
+     *
+     * @param  {number} bitMap - old value
+     * @param  {number} pos - bit position
+     * @param  {number} bitValue - new bit value
+     * @return {number}
+     */
+    static setBit (bitMap: number, pos: number, bitValue: boolean): number {
         const byte = 0x01 << pos;
         const mask = bitValue ? byte : ~byte;
-        return bitValue ? (numValue | mask) : (numValue & mask);
-    }
-
-    static getBit (value: number, pos: number): number {
-        const bit: number = (value >> pos) & 0x01;
-        return bit;
-    }
-
-    static getBitRange (value: number, startPos: number, len: number): number {
-        const mask = Math.pow(2, len) - 1;
-        const range = (value >> startPos) & mask;
-        return range;
+        return bitValue ? (bitMap | mask) : (bitMap & mask);
     }
 
     /**
-     * setBitRange - sets the value in specific the range of bits.
+     * getBit - returns the bit value in specific position.
+     *
+     * @param  {number} bitMap - value
+     * @param  {number} pos - bit position
+     * @return {number}
+     */
+    static getBit (bitMap: number, pos: number): number {
+        return (bitMap >> pos) & 0x01;
+    }
+
+    /**
+     * getBitRange - returns the value in specific range of bits.
+     *
+     * @param  {number} bitMap - old value
+     * @param  {number} startPos - start position
+     * @param  {number} len - number of bits
+     * @return {number}
+     */
+    static getBitRange (bitMap: number, startPos: number, len: number): number {
+        const mask = Math.pow(2, len) - 1;
+        return (bitMap >> startPos) & mask;
+    }
+
+    /**
+     * setBitRange - sets the value in specific range of bits.
      *
      * @param  {number} bitMap - old value
      * @param  {number} newValue - new value for range
@@ -38,11 +59,25 @@ export class TyperUtil {
         return (bitMap & bitMapMask) | newValueMask;
     }
 
+    /**
+     * getByte - returns the byte value in specific position.
+     *
+     * @param  {number} value - value
+     * @param  {number} pos - byte position
+     * @return {number}
+     */
     static getByte (value: number, pos: number): number {
         const byte: number = (value >> (pos * 8)) & 0x0F;
         return byte;
     }
 
+    /**
+     * getWord - returns the word value in specific position.
+     *
+     * @param  {number} value - value
+     * @param  {number} pos - word position
+     * @return {number}
+     */
     static getWord (value: number, pos: number): number {
         const word: number = (value >> (pos * 16)) & 0xFF;
         return word;
