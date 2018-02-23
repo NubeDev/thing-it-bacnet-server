3051e4c36ad66a91d26f17f67d186e877b4590d4
Geass <geass1000@gmail.com>
2018-02-23 12:10:22 +0300

    add: base server logic

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,14 +1,53 @@
-import { IBACnetModule } from './interfaces';
+import * as dgram from 'dgram';
+
+import { IBACnetModule, IBACnetDevice } from './interfaces';
+
+import { ApiError } from './errors';
+import { logger } from './utils';
 
 export class Server {
+    private className: string = 'Server';
+    private port: number;
+    private device: IBACnetDevice;
+    private app: dgram.Socket;
 
-    static bootstrapServer (module: IBACnetModule) {
-        return new Server();
+    static bootstrapServer (bacnetModule: IBACnetModule) {
+        return new Server(bacnetModule);
     }
 
     /**
      * @constructor
+     * @param {IBACnetModule} bacnetModule - module configuration
      */
-    constructor () {
+    constructor (bacnetModule: IBACnetModule) {
+        this.port = bacnetModule.port;
+        this.device = bacnetModule.device;
+        this.app = dgram.createSocket('udp4');
+    }
+
+
+    /**
+     * startServer - starts the server.
+     *
+     * @return {void}
+     */
+    public startServer () {
+        this.app.on('error', () => {
+            ;
+        });
+
+        this.app.on('message', () => {
+            ;
+        });
+
+        this.app.on('listening', () => {
+            const addrInfo = this.app.address();
+            logger.info(`${this.className} - startServer: UDP Server listening on ${addrInfo.address}:${addrInfo.port}`);
+        });
+
+        if (!this.port) {
+            throw new ApiError(`${this.className} - startServer: Port is required!`);
+        }
+        this.app.bind(this.port);
     }
 }
