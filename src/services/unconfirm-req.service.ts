69e4e8d78792620eb5402fd04efb54297b729434
Geass <geass1000@gmail.com>
2018-02-24 16:13:19 +0300

    add: implementation of "iAm" response to the "whoIs" request

--- src/services/unconfirm-req.service.ts
+++ src/services/unconfirm-req.service.ts
@@ -1,12 +1,58 @@
 import {
     BACnetServiceTypes,
+    BLVCFunction,
 } from '../core/enums';
 
 import { RequestSocket, ResponseSocket } from '../core/sockets';
 
+import { unconfirmReqPDU } from '../core/layers/apdus';
+import { blvc, npdu } from '../core/layers';
+
+import { BACnetWriterUtil } from '../core/utils';
+
 export class UnconfirmReqService {
+
+    /**
+     * whoIs - handles the "whoIs" request and sends a "iAm" response.
+     *
+     * @param  {RequestSocket} req - request object (socket)
+     * @param  {ResponseSocket} resp - response object (socket)
+     * @return {type}
+     */
     public whoIs (req: RequestSocket, resp: ResponseSocket) {
-        return;
+        const device = req.device;
+
+        // Generate APDU writer
+        const writerUnconfirmReq = unconfirmReqPDU.writeReq({});
+        const writerIAm = unconfirmReqPDU.writeIAm({
+            objInst: device.id,
+            objType: device.type,
+            vendorId: device.vendorId,
+        });
+        const writerAPDU = BACnetWriterUtil.concat(writerUnconfirmReq, writerIAm);
+
+        // Generate NPDU writer
+        const writerNPDU = npdu.writeNPDULayer({
+            control: {
+                destSpecifier: true,
+            },
+            destNetworkAddress: 0xffff,
+            hopCount: 0xff,
+        });
+
+        // Generate BLVC writer
+        const writerBLVC = blvc.writeBLVCLayer({
+            func: BLVCFunction.originalBroadcastNPDU,
+            npdu: writerNPDU,
+            apdu: writerAPDU,
+        });
+
+        // Concat messages
+        const writerBACnet = BACnetWriterUtil.concat(writerBLVC, writerNPDU, writerAPDU);
+
+        // Get and send BACnet message
+        const msgBACnet = writerBACnet.getBuffer();
+        return resp.send(msgBACnet);
     }
 }
 
