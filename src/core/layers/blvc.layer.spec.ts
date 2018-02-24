61fad70089417e282910e04f00d612cb7cfe3822
Geass <geass1000@gmail.com>
2018-02-24 11:53:16 +0300

    add: tests for "writeBLVCLayer" method

--- src/core/layers/blvc.layer.spec.ts
+++ src/core/layers/blvc.layer.spec.ts
@@ -9,6 +9,14 @@ import { ApiError } from '../errors';
 import { BLVC } from './blvc.layer';
 import { NPDU } from './npdu.layer';
 
+import {
+    BLVCFunction,
+} from '../enums';
+
+import {
+    BACnetWriterUtil,
+} from '../utils';
+
 /* Interfaces */
 
 class NPDUMock {
@@ -45,6 +53,62 @@ describe('BLVC', () => {
                 0xff, 0x00, 0xff, 0x10, 0x00, 0xc4, 0x02, 0x00,
                 0x27, 0x0f, 0x22, 0x05, 0xc4, 0x91, 0x00, 0x21, 0xb2]);
             expect(spyNPDUGetFromBuffer.args[0][0]).to.deep.equal(slicedBuffer);
+        });
+    });
+
+    describe('writeBLVCLayer', () => {
+        let blvc: BLVC;
+
+        beforeEach(() => {
+            blvc = new BLVC(null);
+        });
+
+        it('should return writer with correct signature if APDU and NPDU are not defined', () => {
+            const writer = blvc.writeBLVCLayer({
+                func: BLVCFunction.originalBroadcastNPDU,
+                npdu: null,
+                apdu: null,
+            });
+            const writerBuffer = writer.getBuffer();
+            const proposedBuffer = Buffer.from([0x81, 0x0b, 0x00, 0x04]);
+            expect(writerBuffer).to.deep.equal(proposedBuffer);
+        });
+
+        it('should return writer with correct signature if APDU is not defained and NPDU is defined', () => {
+            const npduWriter = new BACnetWriterUtil(Buffer.from([0x32, 0x55, 0x12]));
+            const writer = blvc.writeBLVCLayer({
+                func: BLVCFunction.originalBroadcastNPDU,
+                npdu: npduWriter,
+                apdu: null,
+            });
+            const writerBuffer = writer.getBuffer();
+            const proposedBuffer = Buffer.from([0x81, 0x0b, 0x00, 0x07]);
+            expect(writerBuffer).to.deep.equal(proposedBuffer);
+        });
+
+        it('should return writer with correct signature if APDU is defained and NPDU is not defined', () => {
+            const apduWriter = new BACnetWriterUtil(Buffer.from([0x32, 0x55]));
+            const writer = blvc.writeBLVCLayer({
+                func: BLVCFunction.originalBroadcastNPDU,
+                npdu: null,
+                apdu: apduWriter,
+            });
+            const writerBuffer = writer.getBuffer();
+            const proposedBuffer = Buffer.from([0x81, 0x0b, 0x00, 0x06]);
+            expect(writerBuffer).to.deep.equal(proposedBuffer);
+        });
+
+        it('should return writer with correct signature if APDU and NPDU are defined', () => {
+            const npduWriter = new BACnetWriterUtil(Buffer.from([0x32, 0x55, 0x12]));
+            const apduWriter = new BACnetWriterUtil(Buffer.from([0x32, 0x55]));
+            const writer = blvc.writeBLVCLayer({
+                func: BLVCFunction.originalBroadcastNPDU,
+                npdu: npduWriter,
+                apdu: apduWriter,
+            });
+            const writerBuffer = writer.getBuffer();
+            const proposedBuffer = Buffer.from([0x81, 0x0b, 0x00, 0x09]);
+            expect(writerBuffer).to.deep.equal(proposedBuffer);
         });
     });
 });
