f7197ca9dd4decce361a00b5f1ac55718d506ae8
Geass <geass1000@gmail.com>
2018-02-24 15:41:50 +0300

    change: set device metadata in request

--- src/core/sockets/request.socket.ts
+++ src/core/sockets/request.socket.ts
@@ -4,14 +4,18 @@ import * as Bluebird from 'bluebird';
 
 import { blvc } from '../layers/blvc.layer';
 
+import { IBACnetDevice } from '../interfaces';
+
 export class RequestSocket {
     public blvc: Map<string, any>;
     public npdu: Map<string, any>;
     public apdu: Map<string, any>;
+    public device: IBACnetDevice;
 
-    constructor (msg: Buffer) {
+    constructor (msg: Buffer, device: IBACnetDevice) {
         this.blvc = blvc.getFromBuffer(msg);
         this.npdu = this.blvc.get('npdu');
         this.apdu = this.npdu.get('apdu');
+        this.device = device;
     }
 }
