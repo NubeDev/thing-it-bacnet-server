17b9bddd10ae6c6eb8761b7e58f11af561f23070
Geass <geass1000@gmail.com>
2018-02-26 09:07:31 +0300

    add: send "broadcast" message

--- src/core/sockets/response.socket.ts
+++ src/core/sockets/response.socket.ts
@@ -1,4 +1,5 @@
 import * as dgram from 'dgram';
+import * as _ from 'lodash';
 
 import * as Bluebird from 'bluebird';
 
@@ -13,6 +14,23 @@ export class ResponseSocket {
         return new Bluebird((resolve, reject) => {
             this.app.send(message, 0, message.length,
                 this.port, this.address, (error, data) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    resolve(data);
+                });
+        });
+    }
+
+    public sendBroadcast (message: Buffer, address?: string, port?: number): Bluebird<any> {
+        this.app.setBroadcast(true);
+        const broadcastAddress = address ? address : '255.255.255.255';
+        const broadcastPort = _.isNumber(port) ? port : 1234;
+
+        return new Bluebird((resolve, reject) => {
+            this.app.send(message, 0, message.length,
+                broadcastPort, broadcastAddress, (error, data) => {
+                    this.app.setBroadcast(false);
                     if (error) {
                         return reject(error);
                     }
