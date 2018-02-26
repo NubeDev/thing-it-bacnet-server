b0eb958fe39e37905455bc5ca2963e6ac9c47bb7
Geass <geass1000@gmail.com>
2018-02-26 15:22:03 +0300

    add: logs

--- src/core/sockets/response.socket.ts
+++ src/core/sockets/response.socket.ts
@@ -1,9 +1,11 @@
 import * as dgram from 'dgram';
 import * as _ from 'lodash';
+import { logger } from '../utils';
 
 import * as Bluebird from 'bluebird';
 
 export class ResponseSocket {
+    public className: string = 'ResponseSocket' ;
 
     constructor (private app: dgram.Socket,
         private port: number,
@@ -11,6 +13,7 @@ export class ResponseSocket {
     }
 
     public send (message: Buffer): Bluebird<any> {
+        logger.debug(`${this.className} - send (${this.address}:${this.port}): ${message.toString('hex')}`);
         return new Bluebird((resolve, reject) => {
             this.app.send(message, 0, message.length,
                 this.port, this.address, (error, data) => {
@@ -27,6 +30,7 @@ export class ResponseSocket {
         const broadcastAddress = address ? address : '255.255.255.255';
         const broadcastPort = _.isNumber(port) ? port : this.port;
 
+        logger.debug(`${this.className} - sendBroadcast (${broadcastAddress}:${broadcastPort}): ${message.toString('hex')}`);
         return new Bluebird((resolve, reject) => {
             this.app.send(message, 0, message.length,
                 broadcastPort, broadcastAddress, (error, data) => {
