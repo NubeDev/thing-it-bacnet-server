ff29ac455a58800594d80cefe5d68ffaad9c9ef1
Geass <geass1000@gmail.com>
2018-02-26 12:43:08 +0300

    add: cov notification request method

--- src/services/unconfirm-req.service.ts
+++ src/services/unconfirm-req.service.ts
@@ -1,5 +1,6 @@
 import {
     BACnetServiceTypes,
+    BACnetPropIds,
     BLVCFunction,
 } from '../core/enums';
 
@@ -53,6 +54,60 @@ export class UnconfirmReqService {
         // Get and send BACnet message
         const msgBACnet = writerBACnet.getBuffer();
         return resp.sendBroadcast(msgBACnet);
+    }
+
+    /**
+     * covNotification - handles the "COV notification" unconfirmed request.
+     *
+     * @param  {RequestSocket} req - request object (socket)
+     * @param  {ResponseSocket} resp - response object (socket)
+     * @return {type}
+     */
+    public covNotification (req: RequestSocket, resp: ResponseSocket) {
+        const apduService = req.apdu.get('service');
+        const subProcessId = apduService.get('subscriberProcessId');
+
+        // Get object identifier
+        const objIdent = apduService.get('objIdent');
+        const objIdentValue = objIdent.get('value');
+        const objType = objIdentValue.get('type');
+        const objInst = objIdentValue.get('instance');
+
+        // Get BACnet object (from device)
+        const device = req.device.getDevice();
+        const bnObject = req.device.getObject(objInst, objType);
+
+        // Get value
+        const bnProp = req.device.getProperty(bnObject, BACnetPropIds.presentValue);
+        const bnStatus = req.device.getProperty(bnObject, BACnetPropIds.statusFlags);
+
+        // Generate APDU writer
+        const writerUnconfirmReq = unconfirmReqPDU.writeReq({});
+        const writerCOVNotification = unconfirmReqPDU.writeCOVNotification({
+            device: device,
+            devObject: bnObject,
+            prop: bnProp,
+            status: bnStatus,
+            processId: subProcessId,
+        });
+        const writerAPDU = BACnetWriterUtil.concat(writerUnconfirmReq, writerCOVNotification);
+
+        // Generate NPDU writer
+        const writerNPDU = npdu.writeNPDULayer({});
+
+        // Generate BLVC writer
+        const writerBLVC = blvc.writeBLVCLayer({
+            func: BLVCFunction.originalUnicastNPDU,
+            npdu: writerNPDU,
+            apdu: writerAPDU,
+        });
+
+        // Concat messages
+        const writerBACnet = BACnetWriterUtil.concat(writerBLVC, writerNPDU, writerAPDU);
+
+        // Get and send BACnet message
+        const msgBACnet = writerBACnet.getBuffer();
+        return resp.send(msgBACnet);
     }
 }
 
