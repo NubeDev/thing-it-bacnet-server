bdb6184c97a9cddf9a1fe295f6b4bc3c2b203b9a
Geass <geass1000@gmail.com>
2018-02-26 10:19:46 +0300

    add: method for getting of object/property from a module

--- src/core/device.ts
+++ src/core/device.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 
-import { IBACnetDevice } from './interfaces';
+import { IBACnetDevice, IBACnetObject, IBACnetObjectProperty } from './interfaces';
 
 export class Device {
     private device: IBACnetDevice;
@@ -15,5 +15,35 @@ export class Device {
 
     public initDevice (device: IBACnetDevice): void {
         this.device = _.cloneDeep(device);
+    }
+
+    /**
+     * getObject - returns the object from BACnet module.
+     *
+     * @param  {number} id - object identifier
+     * @param  {number} type - object type
+     * @return {IBACnetObject}
+     */
+    public getObject (id: number, type: number): IBACnetObject {
+        if (this.device.id === id && this.device.type === type) {
+            return this.device;
+        }
+        const bnObject = _.find(this.device.objects, { 'id': id, 'type': type });
+        return bnObject;
+    }
+
+    /**
+     * getProperty - returns the property from BACnet object.
+     *
+     * @param  {IBACnetObject} bnObject - BACnet object
+     * @param  {number} propId - property identifier
+     * @return {IBACnetObjectProperty}
+     */
+    public getProperty (bnObject: IBACnetObject, propId: number): IBACnetObjectProperty {
+        if (!bnObject) {
+            return null;
+        }
+        const prop = _.find(bnObject.props, [ 'id', propId ]);
+        return _.cloneDeep(prop);
     }
 }
