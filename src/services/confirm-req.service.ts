0e18ab7593a72ddb4985fdfd2feeea7adcc1b9a6
Geass <geass1000@gmail.com>
2018-02-27 12:18:01 +0300

    delete: unused code

--- src/services/confirm-req.service.ts
+++ src/services/confirm-req.service.ts
@@ -5,17 +5,6 @@ import {
 import { RequestSocket, ResponseSocket } from '../core/sockets';
 
 export class ConfirmReqService {
-    public readProperty (req: RequestSocket, resp: ResponseSocket) {
-        return;
-    }
-
-    public writeProperty (req: RequestSocket, resp: ResponseSocket) {
-        return;
-    }
-
-    public subscribeCOV (req: RequestSocket, resp: ResponseSocket) {
-        return;
-    }
 }
 
 export const confirmReqService: ConfirmReqService = new ConfirmReqService();
