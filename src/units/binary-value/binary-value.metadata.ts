617c13e213cac5abed03eb95b61fa191098a75fd
Geass <geass1000@gmail.com>
2018-02-28 15:59:26 +0300

    add: basic binary-value unit

---
+++ src/units/binary-value/binary-value.metadata.ts
@@ -0,0 +1,29 @@
+import {
+    BACnetPropIds,
+    BACnetObjTypes,
+    BACnetPropTypes,
+} from '../../core/enums';
+
+export const BinaryValueMetadata = {
+    id: 0,
+    type: BACnetObjTypes.BinaryValue,
+    props: [
+        {
+            id: BACnetPropIds.presentValue,
+            type: BACnetPropTypes.enumerated,
+            values: {
+                value: 0,
+            },
+        },
+        {
+            id: BACnetPropIds.statusFlags,
+            type: BACnetPropTypes.bitString,
+            values: {
+                inAlarm: false,
+                fault: false,
+                overridden: false,
+                outOfService: false,
+            },
+        },
+    ]
+};
