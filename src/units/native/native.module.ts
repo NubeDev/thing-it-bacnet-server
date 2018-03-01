548bd2574087659489de7358659b34e0493e1668
Geass <geass1000@gmail.com>
2018-03-01 13:57:33 +0300

    chore: add types

--- src/units/native/native.module.ts
+++ src/units/native/native.module.ts
@@ -1,5 +1,5 @@
 import { BinaryValueUnit } from './binary-value/binary-value.unit';
 
-export const NativeModule = new Map([
+export const NativeModule: Map<string, any> = new Map<string, any>([
     [ 'BinaryValue', BinaryValueUnit ],
 ]);
