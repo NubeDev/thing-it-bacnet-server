0a8fbb8b219a6f63210a0b245c488a254e4e9258
Geass <geass1000@gmail.com>
2018-03-01 16:16:33 +0300

    fix: get id from config

--- src/units/native/binary-value/binary-value.unit.ts
+++ src/units/native/binary-value/binary-value.unit.ts
@@ -9,6 +9,7 @@ import {
 } from '../../../core/errors';
 
 import {
+    IBinaryValueUnit,
     IBACnetObject,
 } from '../../../core/interfaces';
 
@@ -17,16 +18,17 @@ import { BinaryValueMetadata } from './binary-value.metadata';
 import { UnitNativeBase } from '../../../core/bases/unit-native.base';
 
 export class BinaryValueUnit extends UnitNativeBase {
+    public className: string = 'BinaryValueUnit';
     public metadata: IBACnetObject;
 
-    constructor (bnUnit: any) {
-        super();
+    constructor (bnUnit: IBinaryValueUnit) {
+        super(bnUnit);
         this.metadata = _.cloneDeep(BinaryValueMetadata);
 
-        if (_.isNil(bnUnit.id)) {
-            throw new ApiError('BinaryValueUnit - constructor: Unit ID is required!');
+        if (_.isNil(bnUnit.config.id)) {
+            throw new ApiError(`${this.className} - constructor: Unit ID is required!`);
         }
-        this.metadata.id = bnUnit.id;
+        this.metadata.id = bnUnit.config.id;
 
         this.setProps(bnUnit.config);
     }
