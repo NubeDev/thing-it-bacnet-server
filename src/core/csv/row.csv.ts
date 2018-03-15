4e658d6f394a2ab29551d9be80f2a2ff2bff9b33
Geass <geass1000@gmail.com>
2018-03-15 14:19:36 +0300

    add: row class for the csv row

---
+++ src/core/csv/row.csv.ts
@@ -0,0 +1,96 @@
+import * as _ from 'lodash';
+
+import { ApiError } from '../errors';
+
+export class CSVRow {
+    private aliases: Map<string, number>;
+    private cells: Array<number|string>;
+
+    constructor () {
+        this.aliases = new Map();
+        this.cells = [];
+    }
+
+    /**
+     * setAlias - sets the alias to the specific cell.
+     *
+     * @param  {number} cellNumber - cell ID
+     * @param  {string} cellAlias - cell alias
+     * @return {CSVRow}
+     */
+    public setAlias (cellNumber: number, cellAlias: string): CSVRow {
+        if (this.aliases.has(cellAlias)) {
+            throw new ApiError(`CSVRow - setAlias: Alias ${cellAlias} is already exist!`);
+        }
+
+        this.aliases.set(cellAlias, cellNumber);
+        return this;
+    }
+
+    /**
+     * setValue - sets the values in a cell by the cell ID or cell alias.
+     *
+     * @param  {number|string} cellInst - cell ID of cell alias
+     * @param  {number|string} cellValue - new valuse of cell
+     * @return {CSVRow}
+     */
+    public setValue (cellInst: number|string, cellValue: number|string): CSVRow {
+        let cellNumber: number = _.isString(cellInst)
+            ? this.aliases.get(cellInst)
+            : cellInst;
+
+        if (!_.isNumber(cellNumber) || !_.isFinite(cellNumber)) {
+            throw new ApiError(`CSVRow - setValue: Cell "${cellInst}" is not exist!`);
+        }
+
+        const newCellValue: number|string = _.isString(cellValue)
+            ? this.escapeString(cellValue)
+            : cellValue;
+
+        this.cells[cellNumber] = newCellValue;
+        return this;
+    }
+
+    /**
+     * getValue - returns the values by cell ID or cell alias.
+     *
+     * @param  {number|string} cellInst - cell ID of cell alias
+     * @return {number|string}
+     */
+    public getValue (cellInst: number|string): number|string {
+        let cellNumber: number = _.isString(cellInst)
+            ? this.aliases.get(cellInst)
+            : cellInst;
+
+        return this.cells[cellNumber];
+    }
+
+    /**
+     * toString - returns the string representation (csv format) of the row.
+     *
+     * @param  {number} numOfCells - number of cells
+     * @return {string}
+     */
+    public toString (numOfCells: number): string {
+        const rowArray: Array<number|string> = new Array(numOfCells).fill('');
+
+        for (const arrIndex in this.cells) {
+            if (!this.cells.hasOwnProperty(arrIndex)) { continue; }
+            rowArray[arrIndex] = this.cells[arrIndex];
+        }
+
+        return rowArray.join(';');
+    }
+
+    /**
+     * escapeString - returns the escaped string in accordance with csv rules.
+     *
+     * @param  {string} str - unescaped string
+     * @return {string}
+     */
+    public escapeString (str: string): string {
+        const escapedString = _.replace(str, '"', '""');
+        return /(;|,|\n)/.test(escapedString)
+            ? `"${escapedString}"` : escapedString;
+    }
+}
