2ee7027d3bc4f81c545a63d6841580dec49798f8
Geass <geass1000@gmail.com>
2018-03-21 19:56:31 +0300

    chore: remove unused import

--- src/routes/confirmed-req.route.ts
+++ src/routes/confirmed-req.route.ts
@@ -8,7 +8,7 @@ import {
     BACnetConfirmedService,
 } from '../core/enums';
 
-import { confirmReqService, simpleACKService, complexACKService } from '../services';
+import { simpleACKService, complexACKService } from '../services';
 
 import { InputSocket, OutputSocket, ServiceSocket } from '../core/sockets';
 
