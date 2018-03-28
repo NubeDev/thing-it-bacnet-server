1ce446e8ad8cf2c0f698568c9766b562dffdda7d
Geass <geass1000@gmail.com>
2018-03-28 11:18:20 +0300

    add: mandatory `binary-input` properties

--- src/units/native/binary-input/binary-input.metadata.ts
+++ src/units/native/binary-input/binary-input.metadata.ts
@@ -2,10 +2,34 @@ import {
     BACnetPropIds,
     BACnetObjTypes,
     BACnetPropTypes,
+    BACnetBinaryPV,
+    BACnetPolarity,
 } from '../../../core/enums';
 
 import {
     IBACnetObjectProperty,
 } from '../../../core/interfaces';
 
-export const BinaryInputMetadata: IBACnetObjectProperty[] = [];
+export const BinaryInputMetadata: IBACnetObjectProperty[] = [
+    {
+        id: BACnetPropIds.outOfService,
+        type: BACnetPropTypes.boolean,
+        payload: {
+            value: false,
+        },
+    },
+    {
+        id: BACnetPropIds.presentValue,
+        type: BACnetPropTypes.enumerated,
+        payload: {
+            value: BACnetBinaryPV.Active,
+        },
+    },
+    {
+        id: BACnetPropIds.polarity,
+        type: BACnetPropTypes.enumerated,
+        payload: {
+            value: BACnetPolarity.Normal,
+        },
+    },
+];
