e09f21f10952ed21ade0834a6820e5f98cb9927a
Geass <geass1000@gmail.com>
2018-03-28 10:51:16 +0300

    add: units enum variables for binary units

--- src/core/enums/index.ts
+++ src/core/enums/index.ts
@@ -4,3 +4,4 @@ export * from './type.enum';
 export * from './service.enum';
 export * from './layer.enum';
 export * from './common.enum';
+export * from './unit.enum';
---
+++ src/core/enums/unit.enum.ts
@@ -0,0 +1,10 @@
+
+export enum BACnetBinaryPV {
+    Inactive = 0,
+    Active = 1,
+}
+
+export enum BACnetPolarity {
+    Normal = 0,
+    Reverse = 1,
+}
