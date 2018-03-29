6ce79ea30166e7590518498a180a1e3ff7eef36f
Geass <geass1000@gmail.com>
2018-03-29 11:54:26 +0300

    add: "BACnetEventState" enum

--- src/core/enums/unit.enum.ts
+++ src/core/enums/unit.enum.ts
@@ -8,3 +8,12 @@ export enum BACnetPolarity {
     Normal = 0,
     Reverse = 1,
 }
+
+export enum BACnetEventState {
+    Normal = 0,
+    Fault = 1,
+    Offnormal = 2,
+    HighLimit = 3,
+    LowLimit = 4,
+    LifeSafetyAlarm = 5,
+}
