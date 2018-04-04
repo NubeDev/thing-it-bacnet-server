e10de5548973f88292118bade4f4d5eeb0e15ede
Geass <geass1000@gmail.com>
2018-04-04 17:27:59 +0300

    add: default `className` property

--- src/core/utils/types/type.base.ts
+++ src/core/utils/types/type.base.ts
@@ -4,6 +4,7 @@ import { BACnetWriterUtil } from '../bacnet-writer.util';
 import { IBACnetTag } from '../../interfaces';
 
 export abstract class BACnetTypeBase {
+    public readonly className: string = 'BACnetTypeBase';
     protected tag: IBACnetTag;
 
     abstract readValue (reader: BACnetReaderUtil, changeOffset?: boolean): void;
