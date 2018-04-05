892e7cdd966f05fa45d4f2c7ae4ceefcd19583d3
Geass <geass1000@gmail.com>
2018-04-05 09:08:33 +0300

    change: use new types

--- src/units/native/binary-value/binary-value.metadata.ts
+++ src/units/native/binary-value/binary-value.metadata.ts
@@ -2,35 +2,21 @@ import {
     BACnetPropIds,
     BACnetObjTypes,
     BACnetPropTypes,
+    BACnetBinaryPV,
+    BACnetPolarity,
+    BACnetEventState,
+    BACnetReliability,
 } from '../../../core/enums';
 
 import {
     IBACnetObjectProperty,
 } from '../../../core/interfaces';
 
+import * as BACnetTypes from '../../../core/utils/types';
+
 export const BinaryValueMetadata: IBACnetObjectProperty[] = [
     {
         id: BACnetPropIds.objectType,
-        type: BACnetPropTypes.enumerated,
-        payload: {
-            value: BACnetObjTypes.BinaryValue,
-        },
-    },
-    {
-        id: BACnetPropIds.presentValue,
-        type: BACnetPropTypes.enumerated,
-        payload: {
-            value: 0,
-        },
-    },
-    {
-        id: BACnetPropIds.statusFlags,
-        type: BACnetPropTypes.bitString,
-        payload: {
-            inAlarm: false,
-            fault: false,
-            overridden: false,
-            outOfService: false,
-        },
+        payload: new BACnetTypes.BACnetEnumerated(BACnetObjTypes.BinaryValue),
     },
 ];
