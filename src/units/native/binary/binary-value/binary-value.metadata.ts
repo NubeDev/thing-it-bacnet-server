843d19108b7d8a2663143d318e901540c6638934
Geass <geass1000@gmail.com>
2018-04-09 14:16:41 +0300

    delete: unused (moved to statusFlags middle)

--- src/units/native/binary/binary-value/binary-value.metadata.ts
+++ src/units/native/binary/binary-value/binary-value.metadata.ts
@@ -53,26 +53,4 @@ export const BinaryValueMetadata: IBACnetObjectProperty[] = [
         id: BACnetPropIds.currentCommandPriority,
         payload: new BACnetTypes.BACnetNull(),
     },
-
-    {
-        id: BACnetPropIds.outOfService,
-        payload: new BACnetTypes.BACnetBoolean(false),
-    },
-    {
-        id: BACnetPropIds.reliability,
-        payload: new BACnetTypes.BACnetEnumerated(BACnetReliability.NoFaultDetected),
-    },
-    {
-        id: BACnetPropIds.eventState,
-        payload: new BACnetTypes.BACnetEnumerated(BACnetEventState.Normal),
-    },
-    {
-        id: BACnetPropIds.statusFlags,
-        payload: new BACnetTypes.BACnetStatusFlags({
-            inAlarm: false,
-            fault: false,
-            overridden: false,
-            outOfService: false,
-        }),
-    },
 ];
