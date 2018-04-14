60ad5614528d6bf283d60cbb487d4f8c8af3f7b1
Geass <geass1000@gmail.com>
2018-04-14 09:54:31 +0300

    add: interface for function entity of custom units

--- src/core/interfaces/unit.interface.ts
+++ src/core/interfaces/unit.interface.ts
@@ -1,1 +1,15 @@
 import { BACnetTagTypes } from '../enums';
+
+export interface ICustomFunction <T> {
+    unit: T;
+    config: ICustomFunctionConfig;
+}
+
+export interface ICustomFunctionConfig {
+    // Min value
+    min: number;
+    // Max value
+    max: number;
+    // Frequency
+    freq: number;
+}
