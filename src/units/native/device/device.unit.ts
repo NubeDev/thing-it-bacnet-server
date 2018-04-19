f37ced4926727ed3a3d79ca27880a7fe3b75fa84
Geass <geass1000@gmail.com>
2018-04-19 19:31:25 +0300

    change: move storage logic to the "initUnit" method

--- src/units/native/device/device.unit.ts
+++ src/units/native/device/device.unit.ts
@@ -20,12 +20,9 @@ import { NativeUnit } from '../native.unit';
 export class DeviceUnit extends NativeUnit {
     public readonly className: string = 'DeviceUnit';
 
-    constructor () {
-        super();
-        this.storage.addUnitStorage(DeviceMetadata);
-    }
-
     public initUnit (edeUnit: IEDEUnit) {
         super.initUnit(edeUnit);
+
+        this.storage.addUnitStorage(DeviceMetadata);
     }
 }
