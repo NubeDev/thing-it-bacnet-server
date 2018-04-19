679ac41c5e5a449abd1342e9810d76048d973b84
Geass <geass1000@gmail.com>
2018-04-19 19:03:23 +0300

    delete: unused "rand" function

--- src/units/custom/function/function.metadata.ts
+++ src/units/custom/function/function.metadata.ts
@@ -1,17 +1,14 @@
+import { BACnetUnitAbbr} from '../../../core/enums';
 
 import { ICustomMetadata } from '../../../core/interfaces';
 
 export const FunctionMetadata: ICustomMetadata[] = [
     {
-        alias: [ 'default', '0', 'rand', 'random' ],
+        alias: [ BACnetUnitAbbr.Default, '0', 'unif', 'uniform', 'uniformDistribution' ],
         config: { min: 0, max: 1, freq: 1000 },
     },
     {
-        alias: [ '1', 'unif', 'uniform', 'uniformDistribution' ],
-        config: { min: 0, max: 1, freq: 1000 },
-    },
-    {
-        alias: [ '2', 'gaus', 'gaussian' ],
+        alias: [ '1', 'gaus', 'gaussian' ],
         config: { min: 0, max: 1, freq: 1000 },
     },
 ];
