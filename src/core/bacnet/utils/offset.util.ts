3bf20701c50085df0b4676d8dd564af69e9517d1
Geass <geass1000@gmail.com>
2018-04-23 12:18:59 +0300

    add: comments to "Offset" utility

--- src/core/bacnet/utils/offset.util.ts
+++ src/core/bacnet/utils/offset.util.ts
@@ -9,22 +9,46 @@ export class OffsetUtil {
         this.curValue = this.isCorrectValue(defValue) ? defValue : 0;
     }
 
-    public setVaule (val: number): void {
-        this.curValue = this.isCorrectValue(val) ? val : this.curValue;
+    /**
+     * setVaule - sets new offset position.
+     *
+     * @param  {number} offPos - new offset position
+     * @return {void}
+     */
+    public setVaule (offPos: number): void {
+        this.curValue = this.isCorrectValue(offPos) ? offPos : this.curValue;
     }
 
+    /**
+     * getVaule - returns current offset position.
+     *
+     * @return {number} - current offset position
+     */
     public getVaule (): number {
         return this.curValue;
     }
 
-    public inc (val?: number): number {
-        const inc = this.isCorrectValue(val) ? val : 1;
+    /**
+     * inc - increases the internal counter of the offset position. If "incValue"
+     * is an incorrect number, method will use a "1" instead of the "incValue".
+     *
+     * @param  {number} incValue - offset from old offset position
+     * @return {number} - old offset position
+     */
+    public inc (incValue?: number): number {
+        const inc = this.isCorrectValue(incValue) ? incValue : 1;
         const oldValue: number = this.curValue;
         this.curValue = this.curValue + inc;
         return oldValue;
     }
 
-    private isCorrectValue (val: number) {
-        return _.isNumber(val) && _.isFinite(val);
+    /**
+     * isCorrectValue - checks if value is a correct number.
+     *
+     * @param  {number} value - number value
+     * @return {boolean}
+     */
+    private isCorrectValue (value: number): boolean {
+        return _.isNumber(value) && _.isFinite(value);
     }
 }
