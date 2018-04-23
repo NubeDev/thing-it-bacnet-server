c18a3e537002d53335a63ac4bf99bc18966d7ceb
Geass <geass1000@gmail.com>
2018-04-23 09:37:45 +0300

    add: new values in layer enums

--- src/core/enums/layer.enum.ts
+++ src/core/enums/layer.enum.ts
@@ -7,12 +7,17 @@ export enum BLVCFunction {
 export enum BACnetConfirmedService {
     SubscribeCOV = 0x05,
     ReadProperty = 0x0c,
+    ReadPropertyMultiple = 0x0e,
     WriteProperty = 0x0f,
+    WritePropertyMultiple = 0x10,
 }
 
 export enum BACnetUnconfirmedService {
     iAm = 0x00,
+    iHave = 0x01,
     covNotification = 0x02,
+    eventNotification = 0x03,
+    whoHas = 0x07,
     whoIs = 0x08,
 }
 
@@ -21,4 +26,8 @@ export enum BACnetServiceTypes {
     UnconfirmedReqPDU = 0x01,
     SimpleACKPDU = 0x02,
     ComplexACKPDU = 0x03,
+    SegmentACKPDU = 0x04,
+    ErrorPDU = 0x05,
+    RejectPDU = 0x06,
+    AbortPDU = 0x07,
 }
