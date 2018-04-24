164c974263a6ad7347062e33089b5c633697a12e
Geass <geass1000@gmail.com>
2018-04-24 10:34:44 +0300

    add: row.csv test file, tests for "constructor" method

---
+++ src/core/csv/row.csv.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import { spy, SinonSpy } from 'sinon';
+
+import { ApiError } from '../errors';
+
+import { CSVRow } from './row.csv';
+
+describe('CSVRow', () => {
+    describe('constructor', () => {
+        it('should create empty cell storage', () => {
+            const row = new CSVRow();
+
+            expect(row['storage']).to.not.be.null;
+            expect(row['storage'].size).to.equal(0);
+        });
+
+        it('should create cell storage with few cells (separator - comma)', () => {
+            const row = new CSVRow('cell1,cell2');
+
+            expect(row['storage']).to.not.be.null;
+            expect(row['storage'].size).to.equal(2);
+        });
+
+        it('should create cell storage with few cells (separator - semicolon)', () => {
+            const row = new CSVRow('cell1;cell2;cell3');
+
+            expect(row['storage']).to.not.be.null;
+            expect(row['storage'].size).to.equal(3);
+        });
+    });
+});
