75f8297f549ce8ccf0d701cdff49c8bc181a1328
Geass <geass1000@gmail.com>
2018-04-24 10:57:43 +0300

    add: tests for "getCellValue" method

--- src/core/csv/row.csv.spec.ts
+++ src/core/csv/row.csv.spec.ts
@@ -38,4 +38,39 @@ describe('CSVRow', () => {
             expect(row['storage']).to.be.null;
         });
     });
+
+    describe('getCellValue', () => {
+        it('should return empty string if cell storage is empty', () => {
+            const row = new CSVRow();
+
+            expect(row.getCellValue(0)).to.equal('');
+        });
+
+        it('should return empty string if value does not exist in cell storage (separator - comma)', () => {
+            const row = new CSVRow('cell1,cell2');
+
+            expect(row.getCellValue(2)).to.equal('');
+        });
+
+        it('should return empty string if value does not exist in cell storage (separator - semicolon)', () => {
+            const row = new CSVRow('cell3;cell4;cell5');
+
+            expect(row.getCellValue(3)).to.equal('');
+        });
+
+        it('should return correct cell values from the cell storage (separator - comma)', () => {
+            const row = new CSVRow('cell1,cell2');
+
+            expect(row.getCellValue(0)).to.equal('cell1');
+            expect(row.getCellValue(1)).to.equal('cell2');
+        });
+
+        it('should return correct cell values from the cell storage (separator - semicolon)', () => {
+            const row = new CSVRow('cell3;cell4;cell5');
+
+            expect(row.getCellValue(0)).to.equal('cell3');
+            expect(row.getCellValue(1)).to.equal('cell4');
+            expect(row.getCellValue(2)).to.equal('cell5');
+        });
+    });
 });
