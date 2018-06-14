1e0ae89696318a1969ef9489cc4d669f8918e7d7
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-14 15:35:04 +0300

    add(runContainer): edeDir argument

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -1,10 +1,11 @@
 import { exec } from 'child_process';
 
-export function runContainer(fileName: string, port: number) {
-    exec( 'docker run --rm --name $FILE -p $PORT:47808/udp -e FILE=$FILE bacnet-server', {
+export function runContainer(fileName: string, port: number, edeDir: string) {
+    exec( 'docker run --rm --name $FILE -v $EDEDIR:/edefiles -p $PORT:47808/udp -e FILE=$FILE bacnet-server', {
         env: {
             'FILE': fileName,
             'PORT': port,
+            'EDEDIR': edeDir,
         },
     }, (error, stdout, stderr) => {
         if (error) {
