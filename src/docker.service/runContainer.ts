b40ac8f1b8206e938ff7f8c2c7ec7c5fa4a7e742
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-14 15:33:17 +0300

    change: using child_process.exec instead of spawn

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -1,11 +1,17 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 
 export function runContainer(fileName: string, port: number) {
-    spawn( 'docker', ['run', '-i', '--rm', '--name', '$FILE', '-p', '$PORT:47808/udp', '-e', 'FILE=$FILE', 'bacnet-server'], {
+    exec( 'docker run --rm --name $FILE -p $PORT:47808/udp -e FILE=$FILE bacnet-server', {
         env: {
             'FILE': fileName,
             'PORT': port,
         },
-        shell: true
-    });
+    }, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
 }
