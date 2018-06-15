b953893a2c8edd4b5a2517b14f11e839410efb61
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-15 11:00:44 +0300

    add(runContainer): event listeners for child process stdout and stderr

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -4,6 +4,12 @@ export function runContainer(fileName: string, port: number, edeDir: string): Ch
     let process: ChildProcess;
     try {
         process = spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
+        process.stdout.on('data', (data) => {
+            console.log(`${fileName}: ${data}`)
+        });
+        process.stderr.on('data', (data) => {
+            console.log(`${fileName}: ${data}`)
+        });
     } catch (error) {
         throw error;
     };
