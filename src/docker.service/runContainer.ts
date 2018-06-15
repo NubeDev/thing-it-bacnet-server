816048339095f793fcb40c7aa304738b2c853e40
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-15 10:59:38 +0300

    add: try - catch for spawning a child process

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -1,6 +1,11 @@
 import { spawn, ChildProcess } from 'child_process';
 
 export function runContainer(fileName: string, port: number, edeDir: string): ChildProcess {
-    let process: ChildProcess = spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
+    let process: ChildProcess;
+    try {
+        process = spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
+    } catch (error) {
+        throw error;
+    };
     return process;
 }
