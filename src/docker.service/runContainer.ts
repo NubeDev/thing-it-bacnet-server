6b1d1b6bba81cbba16584b53e2a24b1443c7f43a
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-15 10:56:46 +0300

    change(runContainer): use spawn and string template instead of env vars

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -1,18 +1,6 @@
-import { exec } from 'child_process';
+import { spawn, ChildProcess } from 'child_process';
 
 export function runContainer(fileName: string, port: number, edeDir: string) {
-    exec( 'docker run --rm --name $FILE -v $EDEDIR:/edefiles -p $PORT:47808/udp -e FILE=$FILE bacnet-server', {
-        env: {
-            'FILE': fileName,
-            'PORT': port,
-            'EDEDIR': edeDir,
-        },
-    }, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
+    spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
 }
+
