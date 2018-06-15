66302117f15211555ac70a720446b068095a0d26
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-15 10:58:32 +0300

    add(runContainer): assign process to var and return it from function

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -1,6 +1,6 @@
 import { spawn, ChildProcess } from 'child_process';
 
-export function runContainer(fileName: string, port: number, edeDir: string) {
-    spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
+export function runContainer(fileName: string, port: number, edeDir: string): ChildProcess {
+    let process: ChildProcess = spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
+    return process;
 }
-
