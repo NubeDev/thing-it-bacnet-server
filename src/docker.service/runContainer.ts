74f82a87713c968ee8c0193d3d04577adf2bf59a
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-18 13:11:21 +0300

    fix: logging debug messages to container.log instead of errors.log

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -5,16 +5,22 @@ export function runContainer(fileName: string, port: number, edeDir: string): Ch
     let containerProcess: ChildProcess;
     try {
         containerProcess = spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
-        containerProcess.stdout.on('data', (data) => {
-            console.log(`${fileName}: ${data}`)
-        });
-        containerProcess.stderr.on('data', (data) => {
-            console.log(`${fileName}: ${data}`)
-        });
         const containerLog = fs.createWriteStream(`./logs/${fileName}.container.log`);
         containerProcess.stdout.pipe(containerLog);
         const containerErrorsLog = fs.createWriteStream(`./logs/${fileName}.container.errors.log`);
-        containerProcess.stderr.pipe(containerErrorsLog);
+
+        containerProcess.stdout.on('data', (data) => {
+            console.log(`${fileName}: ${data}`)
+        });
+        containerProcess.stderr.on('data', (data: string) => {
+            console.log(`${fileName}: ${data}`);
+            if (data.includes('error')) {
+                containerErrorsLog.write(data)
+            } else {
+                containerLog.write(data);
+            }
+        });
+
     } catch (error) {
         throw error;
     };
