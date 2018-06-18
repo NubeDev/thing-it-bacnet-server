9ab4c4aa2ce1152947c290c91a7cda43a21936a1
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-18 11:54:43 +0300

    fix: better naming

--- src/docker.service/runContainer.ts
+++ src/docker.service/runContainer.ts
@@ -2,21 +2,21 @@ import { spawn, ChildProcess } from 'child_process';
 import * as fs from 'fs';
 
 export function runContainer(fileName: string, port: number, edeDir: string): ChildProcess {
-    let process: ChildProcess;
+    let containerProcess: ChildProcess;
     try {
-        process = spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
-        process.stdout.on('data', (data) => {
+        containerProcess = spawn( 'docker', ['run', '-i', '--rm', '--name', `${fileName}`, '-v', `${edeDir}:/edefiles`, '-p', `${port}:47808/udp`, '-e', `FILE=${fileName}`, 'bacnet-server']);
+        containerProcess.stdout.on('data', (data) => {
             console.log(`${fileName}: ${data}`)
         });
-        process.stderr.on('data', (data) => {
+        containerProcess.stderr.on('data', (data) => {
             console.log(`${fileName}: ${data}`)
         });
         const containerLog = fs.createWriteStream(`./logs/${fileName}.container.log`);
-        process.stdout.pipe(containerLog);
+        containerProcess.stdout.pipe(containerLog);
         const containerErrorsLog = fs.createWriteStream(`./logs/${fileName}.container.errors.log`);
-        process.stderr.pipe(containerErrorsLog);
+        containerProcess.stderr.pipe(containerErrorsLog);
     } catch (error) {
         throw error;
     };
-    return process;
+    return containerProcess;
 }
