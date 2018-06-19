b25d740da42a5302e042e668ef91492aa342d662
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-19 10:29:13 +0300

    rename container.manager to index (already in container manager folder)

--- src/addons/docker/index.ts
+++ src/addons/docker/index.ts
@@ -1,4 +1,4 @@
 export { Container } from './containers.manager/container';
-export { ContainersManager } from './containers.manager/containers.manager';
+export { ContainersManager } from './containers.manager/index';
 export { ProxyUDPServer } from './proxy.server';
 export { Service } from './service';
