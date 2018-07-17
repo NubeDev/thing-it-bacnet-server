05fa6ed6b3017f9acdf6561cec543593ed0c7695
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-07-17 13:09:16 +0300

    change: complexACKRouter
    use bacnet-logic package

--- src/routes/complex-ack.route.ts
+++ src/routes/complex-ack.route.ts
@@ -1,22 +1,24 @@
 import { logger } from '../core/utils';
 
-import {
-    ILayerComplexACK,
-} from '../core/bacnet/interfaces';
+// import {
+//     ILayerComplexACK,
+// } from '../core/interfaces';
 
-import {
-    BACnetConfirmedService,
-    BACnetPropertyId,
-} from '../core/bacnet/enums';
+// import {
+//     BACnetConfirmedService,
+//     BACnetPropertyId,
+// } from '../core/bacnet/enums';
+
+import * as BACNet from 'tid-bacnet-logic';
 
 import { InputSocket, OutputSocket, ServiceSocket } from '../core/sockets';
 
 export function ComplexACKRouter (
         inputSoc: InputSocket, outputSoc: OutputSocket, serviceSocket: ServiceSocket) {
-    const apduMessage = inputSoc.apdu as ILayerComplexACK;
+    const apduMessage = inputSoc.apdu as BACNet.Interfaces.ComplexACK.Read.Layer;
     const serviceChoice = apduMessage.serviceChoice;
 
-    logger.debug(`MainRouter - Request Service: ${BACnetConfirmedService[serviceChoice]}`);
+    logger.debug(`MainRouter - Request Service: ${BACNet.Enums.ConfirmedServiceChoice[serviceChoice]}`);
     switch (serviceChoice) {
     }
     return;
