96ba9c72c44e390a6628bb6a6e60e49ff8f02385
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-07-17 13:13:06 +0300

    change: SImpleACKRouter
    using bacnet-logic

--- src/routes/simple-ack.route.ts
+++ src/routes/simple-ack.route.ts
@@ -1,21 +1,23 @@
 import { logger } from '../core/utils';
 
-import {
-    ILayerSimpleACK,
-} from '../core/bacnet/interfaces';
+// import {
+//     ILayerSimpleACK,
+// } from '../core/interfaces';
 
-import {
-    BACnetConfirmedService,
-} from '../core/bacnet/enums';
+// import {
+//     BACnetConfirmedService,
+// } from '../core/bacnet/enums';
 
 import { InputSocket, OutputSocket, ServiceSocket } from '../core/sockets';
 
+import * as BACNet from 'tid-bacnet-logic';
+
 export function SimpleACKRouter (
         inputSoc: InputSocket, outputSoc: OutputSocket, serviceSocket: ServiceSocket) {
-    const apduMessage = inputSoc.apdu as ILayerSimpleACK;
+    const apduMessage = inputSoc.apdu as BACNet.Interfaces.SimpleACK.Read.Layer;
     const serviceChoice = apduMessage.serviceChoice;
 
-    logger.debug(`MainRouter - Request Service: ${BACnetConfirmedService[serviceChoice]}`);
+    logger.debug(`MainRouter - Request Service: ${BACNet.Enums.ConfirmedServiceChoice[serviceChoice]}`);
     switch (serviceChoice) {
     }
     return;
