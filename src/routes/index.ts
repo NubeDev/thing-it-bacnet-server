fc0cf00c4ef1a5f15adb979dd17884ac0c9b8e33
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-07-17 13:05:16 +0300

    change: main router
    using bacnet-logic

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
 import { logger } from '../core/utils';
 
-import {
-    BACnetServiceTypes,
-} from '../core/bacnet/enums';
+// import {
+//     BACnetServiceTypes,
+// } from '../core/bacnet/enums';
 
 import { ConfirmedReqRouter } from './confirmed-req.route';
 import { UnconfirmedReqRouter } from './unconfirmed-req.route';
@@ -11,20 +11,22 @@ import { ComplexACKRouter } from './complex-ack.route';
 
 import { InputSocket, OutputSocket, ServiceSocket } from '../core/sockets';
 
+import * as BACNet from 'tid-bacnet-logic';
+
 export function mainRouter (
         inputSoc: InputSocket, outputSoc: OutputSocket, serviceSocket: ServiceSocket) {
     const apduReq = inputSoc.apdu;
     const pduType = apduReq.type;
 
-    logger.debug(`MainRouter - Request PDU: ${BACnetServiceTypes[pduType]}`);
+    logger.debug(`MainRouter - Request PDU: ${BACNet.Enums.ServiceType[pduType]}`);
     switch (pduType) {
-        case BACnetServiceTypes.ConfirmedReqPDU:
+        case BACNet.Enums.ServiceType.ConfirmedReqPDU:
             return ConfirmedReqRouter(inputSoc, outputSoc, serviceSocket);
-        case BACnetServiceTypes.UnconfirmedReqPDU:
+        case BACNet.Enums.ServiceType.UnconfirmedReqPDU:
             return UnconfirmedReqRouter(inputSoc, outputSoc, serviceSocket);
-        case BACnetServiceTypes.SimpleACKPDU:
+        case BACNet.Enums.ServiceType.SimpleACKPDU:
             return SimpleACKRouter(inputSoc, outputSoc, serviceSocket);
-        case BACnetServiceTypes.ComplexACKPDU:
+        case BACNet.Enums.ServiceType.ComplexACKPDU:
             return ComplexACKRouter(inputSoc, outputSoc, serviceSocket);
     }
     return;
