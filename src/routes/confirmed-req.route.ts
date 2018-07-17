7da667eea1075acc93649fb85f94a321f8059620
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-07-17 13:11:36 +0300

    change: ConfirmedReqRouter
    use bacnet-logic package

--- src/routes/confirmed-req.route.ts
+++ src/routes/confirmed-req.route.ts
@@ -1,29 +1,31 @@
 import { logger } from '../core/utils';
 
-import {
-    ILayerConfirmedReq,
-} from '../core/bacnet/interfaces';
+// import {
+//     ILayerConfirmedReq,
+// } from '../core/interfaces';
 
-import {
-    BACnetConfirmedService,
-} from '../core/bacnet/enums';
+// import {
+//     BACnetConfirmedService,
+// } from '../core/bacnet/enums';
 
 import { unitConfirmedReqService } from '../services';
 
 import { InputSocket, OutputSocket, ServiceSocket } from '../core/sockets';
 
+import * as BACNet from 'tid-bacnet-logic';
+
 export function ConfirmedReqRouter (
         inputSoc: InputSocket, outputSoc: OutputSocket, serviceSocket: ServiceSocket) {
-        const apduMessage = inputSoc.apdu as ILayerConfirmedReq;
+        const apduMessage = inputSoc.apdu as BACNet.Interfaces.ConfirmedRequest.Read.Layer;
         const serviceChoice = apduMessage.serviceChoice;
 
-    logger.debug(`MainRouter - Request Service: ${BACnetConfirmedService[serviceChoice]}`);
+    logger.debug(`MainRouter - Request Service: ${BACNet.Enums.ConfirmedServiceChoice[serviceChoice]}`);
     switch (serviceChoice) {
-        case BACnetConfirmedService.ReadProperty:
+        case BACNet.Enums.ConfirmedServiceChoice.ReadProperty:
             return unitConfirmedReqService.readProperty(inputSoc, outputSoc, serviceSocket);
-        case BACnetConfirmedService.WriteProperty:
+        case BACNet.Enums.ConfirmedServiceChoice.WriteProperty:
             return unitConfirmedReqService.writeProperty(inputSoc, outputSoc, serviceSocket);
-        case BACnetConfirmedService.SubscribeCOV:
+        case BACNet.Enums.ConfirmedServiceChoice.SubscribeCOV:
             return unitConfirmedReqService.subscribeCOV(inputSoc, outputSoc, serviceSocket);
     }
     return;
