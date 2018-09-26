4671b3ca62a403ecfc3be15c4c8b78069685be00
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-09-26 15:30:55 +0300

    add: multi-state value logic

--- src/units/native/multi-state/multi-state-value/multi-state-value.metadata.ts
+++ src/units/native/multi-state/multi-state-value/multi-state-value.metadata.ts
@@ -9,4 +9,12 @@ export const MultiStateValueMetadata: UnitStorageProperty[] = [
         id: BACNet.Enums.PropertyId.objectType,
         payload: new BACNet.Types.BACnetEnumerated(BACNet.Enums.ObjectType.MultiStateValue),
     },
+    {
+        id: BACNet.Enums.PropertyId.relinquishDefault,
+        payload: new BACNet.Types.BACnetUnsignedInteger(1),
+    },
+    {
+        id: BACNet.Enums.PropertyId.presentValue,
+        payload: new BACNet.Types.BACnetUnsignedInteger(1),
+    },
 ];
