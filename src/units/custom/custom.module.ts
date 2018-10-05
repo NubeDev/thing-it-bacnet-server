69ab5c51ecbdb089fb2ae52f1829f43f2e4d076a
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-10-05 17:37:06 +0300

    fix: thermostat unit usage in `custom` module

--- src/units/custom/custom.module.ts
+++ src/units/custom/custom.module.ts
@@ -3,6 +3,7 @@ import { BACnetUnitAbbr } from '../../core/enums';
 import { CustomUnit } from './custom.unit';
 
 import { FunctionUnit } from './function/function.unit';
+import { ThermostatUnit } from './thermostat/thermostat.unit';
 import { NoopUnit } from './noop/noop.unit';
 
 export const CustomModule: AliasMap<any> = new AliasMap<any>([
@@ -16,6 +17,6 @@ export const CustomModule: AliasMap<any> = new AliasMap<any>([
     },
     {
         alias: [ '2', 'th', 'thermo', 'thermostat' ],
-        value: FunctionUnit,
+        value: ThermostatUnit,
     }
 ]);
