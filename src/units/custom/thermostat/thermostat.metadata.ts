862cd04de3913a387f91799e964ae68ae1e03f2d
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-10-08 16:51:48 +0300

    add: more aliases for thermostat functions

--- src/units/custom/thermostat/thermostat.metadata.ts
+++ src/units/custom/thermostat/thermostat.metadata.ts
@@ -1,21 +1,52 @@
 import { BACnetThermostatUnitFunctions } from '../../../core/enums';
 import { IThermostatMetadata } from '../../../core/interfaces';
 
+export const SetpointFeedbackAliases: string[] = [
+    BACnetThermostatUnitFunctions.SetpointFeedback,
+    '0',
+    'setpoint-feedback',
+    'setpointFb',
+    'setpoint-fb',
+    'feedback',
+    'fb'
+];
+
+export const SetpointModificationAliases: string[] = [
+    BACnetThermostatUnitFunctions.SetpointModification,
+    '1',
+    'setpoint-modification',
+    'setpointMod',
+    'setpoint-mod',
+    'modification',
+    'modif'
+];
+
+export const TemperatureAliases: string[] = [
+    BACnetThermostatUnitFunctions.Temperature,
+    '2',
+    'temp'
+];
+
+export const ModeAliases: string[] = [
+    BACnetThermostatUnitFunctions.Mode,
+    '3'
+];
+
 export const ThermostatMetadata: IThermostatMetadata[] = [
     {
-        alias: [ BACnetThermostatUnitFunctions.SetpointFeedback ],
+        alias: SetpointFeedbackAliases,
         config: { min: 18, max: 26 },
     },
     {
-        alias: [ BACnetThermostatUnitFunctions.SetpointModification ],
+        alias: SetpointModificationAliases,
         config: { min: -4, max: 4 },
     },
     {
-        alias: [ BACnetThermostatUnitFunctions.Temperature],
+        alias: TemperatureAliases,
         config: { min: 0, max: 40, freq: 1000 },
     },
     {
-        alias: [ BACnetThermostatUnitFunctions.Mode ],
+        alias: ModeAliases,
         config: { stateText: [ 'COOL', 'HEAT' ] },
     }
 ];
