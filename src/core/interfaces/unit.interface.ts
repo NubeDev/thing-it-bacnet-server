a539d826563a692392810675d49f2d988e00211e
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-10-10 11:28:48 +0300

    add: light unit interfaces

--- src/core/interfaces/unit.interface.ts
+++ src/core/interfaces/unit.interface.ts
@@ -125,6 +125,38 @@ export namespace Units {
         }
     }
 
+    /**
+     * Light
+     */
+    export namespace Light {
+        export interface Metadata {
+            alias: string|string[];
+            config: Level.Config|State.Config;
+        }
+
+        export namespace Level {
+            export interface Config {
+                min: number;
+                max: number;
+            }
+
+            export interface Function <T> {
+                unit: T;
+                config: Config;
+            }
+        }
+
+        export namespace State {
+            export interface Config {
+                stateText: string[];
+            }
+
+            export interface Function <T> {
+                unit: T;
+                config: Config;
+            }
+        }
+    }
 }
 
 
