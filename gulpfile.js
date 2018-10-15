01d47b9f19fa76e11fe4dd6019924a9f0552ce25
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-10-15 12:29:44 +0300

    fix: update gulpfile acording to gulp v4.0 api

--- gulpfile.js
+++ gulpfile.js
@@ -22,16 +22,16 @@ gulp.task('clean:test', () => {
         .pipe(clean({ force: true }));
 });
 
-gulp.task('build:code', ['clean:code'], () => {
+gulp.task('build:code', gulp.series( 'clean:code', () => {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest(folderApp));
-});
-gulp.task('build:test', ['clean:test'], () => {
+}));
+gulp.task('build:test', gulp.series( 'clean:test', () => {
     return tsTest.src()
         .pipe(tsTest())
         .js.pipe(gulp.dest(folderTests));
-});
+}));
 
 gulp.task('start:test:unit', () => {
     return gulp.src([ `${folderTests}/**/*.spec.js`, `!${folderTests}/thing-it-tests/**/*.spec.js` ])
@@ -61,31 +61,31 @@ gulp.task('tslint:code', () =>
         .pipe(tslint.report())
 );
 
-gulp.task('watch:test:unit', [ 'tslint:test', 'build:test' ], () => {
+gulp.task('watch:test:unit', gulp.series(  'tslint:test', 'build:test' , () => {
     runSequence('start:test:unit');
     gulp.watch([ `${folderSrc}/**/*.ts` ], () => {
         runSequence('tslint:test', 'build:test', 'start:test:unit');
     });
-});
-gulp.task('watch:test:mock', [ 'tslint:test', 'build:test' ], () => {
+}));
+gulp.task('watch:test:mock', gulp.series(  'tslint:test', 'build:test' , () => {
     runSequence('start:test:mock');
     gulp.watch([ `${folderSrc}/**/*.ts` ], () => {
         runSequence('tslint:test', 'build:test', 'start:test:mock');
     });
-});
+}));
 
-gulp.task('watch:build:test', [ 'tslint:test', 'build:test' ], () => {
+gulp.task('watch:build:test', gulp.series(  'tslint:test', 'build:test' , () => {
     gulp.watch([ `${folderSrc}/**/*.ts` ], () => {
         runSequence('tslint:test', 'build:test');
     });
-});
+}));
 
-gulp.task('watch:build:code', [ 'tslint:code', 'build:code' ], () => {
+gulp.task('watch:build:code', gulp.series(  'tslint:code', 'build:code' , () => {
     gulp.start('start');
     gulp.watch([ `${folderSrc}/**/*.ts`, '!./node_modules/**/*', `!${folderSrc}/**/*.spec.ts` ], () => {
         runSequence('tslint:code', 'build:code', 'start');
     });
-});
+}));
 
 let spawn = require('child_process').spawn;
 let node;
